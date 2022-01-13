c5e91d0a32bda2eef73bbcf2be0e5a053564cbda
Christoph Rumpel <christoph@christoph-rumpel.com>
2022-01-13 14:11:00 +0100

    Fix wrong twitter config names

--- src/TwitterWrapper.php
+++ src/TwitterWrapper.php
@@ -42,10 +42,10 @@ class TwitterWrapper
 //        dd(config('services.twitter'));
         app()->singleton(TwitterOAuth::class, function(): TwitterOAuth {
             return new TwitterOAuth(
-                (string) config('services.twitter.consumer_key'),
-                (string) config('services.twitter.consumer_secret'),
-                (string) config('services.twitter.access_token'),
-                (string) config('services.twitter.access_token_secret')
+                (string) config('services.twitter.consumer-key'),
+                (string) config('services.twitter.consumer-secret'),
+                (string) config('services.twitter.access-token'),
+                (string) config('services.twitter.access-token-secret')
             );
         });
     }
