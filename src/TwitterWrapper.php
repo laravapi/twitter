0aa7116bbf0be6afa259ca54629d8afbb94fb3bb
Christoph Rumpel <christoph@christoph-rumpel.com>
2022-01-13 11:31:10 +0100

    Add boot method to register client api instance and add all config details

--- src/TwitterWrapper.php
+++ src/TwitterWrapper.php
@@ -2,6 +2,8 @@
 
 namespace Laravapi\Twitter;
 
+use Abraham\TwitterOAuth\TwitterOAuth;
+
 class TwitterWrapper
 {
     public function neededCredentials()
@@ -19,21 +21,32 @@ class TwitterWrapper
         return [
             'TWITTER_CONSUMER_KEY' => 'services.twitter.consumer-key',
             'TWITTER_CONSUMER_SECRET' => 'services.twitter.consumer-secret',
+            'TWITTER_ACCESS_TOKEN' => 'services.twitter.access-token',
+            'TWITTER_ACCESS_TOKEN_SECRET' => 'services.twitter.access-token-secret',
         ];
     }
 
-    /**
-     * @method tweet Tweet a tweet
-     * @method follower Get followers
-     */
-    public function tweet($a, $b)
+    public function tweet(string $status)
     {
-        dump($a);
-        dump($b);
+        dd(app(TwitterOAuth::class)->post("statuses/update", ["status" => $status]));
     }
 
-    public function testTweet()
+    public function __call(string $name, array $arguments)
     {
+       app(TwitterOAuth::class)->$name(...$arguments);
+    }
 
+
+    public function boot()
+    {
+//        dd(config('services.twitter'));
+        app()->singleton(TwitterOAuth::class, function(): TwitterOAuth {
+            return new TwitterOAuth(
+                (string) config('services.twitter.consumer_key'),
+                (string) config('services.twitter.consumer_secret'),
+                (string) config('services.twitter.access_token'),
+                (string) config('services.twitter.access_token_secret')
+            );
+        });
     }
 }
